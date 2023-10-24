743ec9cff71126d0daa57fad088214f571c331e5
Culf-B <49363745+Culf-B@users.noreply.github.com>
2023-10-24 19:57:32 +0200

    Added functions for loading level and asset data

--- sketch.js
+++ sketch.js
@@ -1,6 +1,36 @@
 let player;
+
+const assets = [];
+const levels = [];
+
 const sW = 800;
 const sH = 800;
+
+// Functions for dataloading
+function loadAssets(jsonData) {
+  /*
+  This function loads assetdata and pushes to "assets"
+  */
+  jsonData.assets.forEach(asset => {
+    assets.push({"name": asset.name, "file": loadImage("assets/" + asset.file)});
+  });
+}
+function loadLevels(jsonData) {
+    // Load and push leveldata
+    levels.push({
+      key: jsonData.name,
+      value: jsonData.layers
+    });
+}
+
+function preload() {
+  /*
+  Preload level and assetdata
+  */
+  // Preload asset files
+  loadJSON("assets/assets.json", loadAssets);
+  // Preload level files
+}
 
 function setup() {
   createCanvas(sW, sH);
