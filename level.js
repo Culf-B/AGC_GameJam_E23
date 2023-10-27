7d18de071d9a0f6f4fc1044d4a74e57532b453e9
Culf-B <49363745+Culf-B@users.noreply.github.com>
2023-10-27 12:35:12 +0200

    Temporarly removed unstable fly animation

--- level.js
+++ level.js
@@ -2,7 +2,6 @@ class Level {
     constructor(w, h, levelData, allAssetData, player) {
         this.sW = w;
         this.sH = h;
-        this.Flymove=1;
         
         this.levelData = levelData;
         this.player = player
@@ -28,12 +27,6 @@ class Level {
 
     update(delta) {
         this.player.update(delta, this);
-        //fly animation we can spam for the level to feel more alive
-        this.Flyanimation=[];
-        for (let i=0;i<25;i++){
-            this.Flyanimation.push(assets[`FlyAnimation` + (i+1)].file)   
-        }
-        this.Flyanimation.push(assets[`FlyAnimation1`].file)
     }
     draw() {
         if (this.player.x >= 0 && this.player.x <= this.levelWidth - this.sW) {
@@ -58,10 +51,6 @@ class Level {
 
         }
         */
-        this.Flymove+=1;
-        if (this.Flymove>=24){
-            this.Flymove=1;
-        }
         console.log(this.player.x);
         // Background layers
         this.backgroundLayers.forEach(layer => {
@@ -75,7 +64,6 @@ class Level {
         this.foregroundLayers.forEach(layer => {
             image(this.allAssets[layer.name].file, this.envX * layer.scrollfactor + layer.startPos[0], layer.startPos[1], (this.sH * this.allAssets[layer.name].file.width) / this.allAssets[layer.name].file.height, this.sH);
             //fly animation dimmeren får spillet til at crash når man kommer på stage 2
-            image(this.Flyanimation[round(this.Flymove)],this.envX * layer.scrollfactor+880,380,150,150)
         });
     }
 }
